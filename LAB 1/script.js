88ce331ae9a94312eb6c71ff88a44fdbfc5bfc2c
anasaarjane12 <anasaarjane1@gmail.com>
2026-05-17 19:40:47 +0200

    Added new HTML file

--- LAB 1/script.js
+++ LAB 1/script.js
@@ -1,5 +1,20 @@
 const button = document.getElementById("changeTextButton");
 
+/* 
+  getElementById()
+  finds element using id
+*/
+
+
+
 button.addEventListener("click", function () {
   alert("You clicked the button!");
 });
+
+/* 
+  addEventListener("click")
+  runs code when button clicked
+
+  alert()
+  popup message
+*/
